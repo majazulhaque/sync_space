084a1de1810501e1a44eb2feea14ebf2e6c9e2d5
Majaz <majazulhaque123@gmail.com>
2024-09-07 10:04:21 +0530

    add stream for meet, and set up action api,and stream client provider

--- components/MeetingTypeList.tsx
+++ components/MeetingTypeList.tsx
@@ -10,7 +10,9 @@ const MeetingTypeList = () => {
     "isScheduleMeeting" | "isJoiningMeeting" | "isInstantMeeting" | undefined
   >(undefined);
 
-  const createMeeting = () => {};
+  const createMeeting = () => {
+    
+  };
 
   return (
     <section className="grid grid-cols-1 gap-5 md:grid-cols-2 xl:grid-cols-4">
