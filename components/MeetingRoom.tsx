66bfbb4e191057c20a8714e365d4c4a5abf117b9
Majaz <majazulhaque123@gmail.com>
2024-09-07 22:25:16 +0530

    complete schedule meeting component

--- components/MeetingRoom.tsx
+++ components/MeetingRoom.tsx
@@ -64,7 +64,7 @@ const MeetingRoom = () => {
         </div>
       </div>
       {/* video layout and call controls */}
-      <div className="fixed bottom-0 max-md:bottom-5 flex w-full items-center justify-center gap-5 max-md:gap-2 flex-wrap">
+      <div className="fixed bottom-0 max-md:bottom-5 flex w-full items-center justify-center gap-5 flex-wrap">
         <CallControls onLeave={() => router.push(`/`)} />
 
         <DropdownMenu>
